5890142fb548093238e24a798ed1bdd1f4f690ec
Alex Maltsev <57605252+MasterSancho@users.noreply.github.com>
2023-02-17 22:01:48 +0200

    023-->104->Fetch Single Listing

--- my-house-market-place/src/App.js
+++ my-house-market-place/src/App.js
@@ -11,6 +11,8 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+
 const App = () => {
  return (
   <>
@@ -26,6 +28,7 @@ const App = () => {
      <Route path='/sign-up' element={<SignUp />} />
      <Route path='/forgot-password' element={<ForgotPassword />} />
      <Route path='/create-listing' element={<CreateListing />} />
+     <Route path='/category/:categoryName/:listingId' element={<Listing />} />
     </Routes>
     <Navbar />
    </Router>
