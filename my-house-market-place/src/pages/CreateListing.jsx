115d83421e297eb849d6df43587a5aeb432db6ae
Alex Maltsev <57605252+MasterSancho@users.noreply.github.com>
2023-05-14 11:31:52 +0300

    034--Update

--- my-house-market-place/src/pages/CreateListing.jsx
+++ my-house-market-place/src/pages/CreateListing.jsx
@@ -92,13 +92,13 @@ const CreateListing = () => {
 
   if (geolocationEnabled) {
    const response = await fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=$AIzaSyCNYRiNeUGODLoFznmHcdv9fGloz06EuLI`
    );
 
    const data = await response.json();
 
    geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
-   geolocation.lng = data.results[0]?.geometry.location.lat ?? 0;
+   geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
 
    location =
     data.status === 'ZERO_RESULTS'
